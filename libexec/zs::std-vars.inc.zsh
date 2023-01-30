8049ef1b96a7e8d7911b005c60660dddae458f64
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2023-01-30 16:41:44 +0059

    fix: various fixes, like (#s)/(#e) anhoring instead of #%

--- libexec/zs::std-vars.inc.zsh
+++ libexec/zs::std-vars.inc.zsh
@@ -1,6 +1,8 @@
 setopt rcquotes
 local \
+    qd='$' qa='`' qaa="'" \
     qocb='{' qccb='}' \
+    qob='(' qcb=')' \
     qsp='[[:space:]]##' \
     qosp='[[:space:]]#' \
     qnosp='[^[:space:]]##' qnoosp='[^[:space:]]#' \
@@ -8,8 +10,25 @@ local \
     qinc=/ \
     qnoalnum='[^[:alnum:]]##' \
     qnooalnum='[^[:alnum:]]#' \
-    qstr='(#B)(["]([^"]#)["]|['']([^'']#)['']|([^[:space:]]##))' \
-    qostr='(#B)(["]([^"]#)["]|['']([^'']#)['']|([^[:space:]]#))' \
     qid='[[:IDENT:]]##' \
-    qoid='[[:IDENT:]]#'
- 
+    qoid='[[:IDENT:]]#' \
+    qqstr1='["]([^"]#)["]' \
+    qqstr3='['']([^'']#)['']' \
+    qqstr4='[\`]([^\`]#)[\`]' \
+    qqstr5='$[\(][\(]([^\)]#)[\)][\)]' \
+    qqstr5a='$[\(]([^\)]#)[\)]' \
+    qqstr7='$[\{]([^\}]#)[\}]'
+
+local qstr="($qqstr1|$qqstr3$qqstr4|$qqstr5|$qqstr5a|$qqstr7|)"
+local qostr=$qstr[1,-2]'|([^[:space:]]#))'
+local -a qqprecmd=('\{' '\(' noglob command exec nocorrect builtin
+                    '\&\&' '\|\|' '\|' '\&' if while )
+local qpre="$qosp(${(~j.|.)${qqprecmd[@]/(#e)/$qsp}}')"
+local qemu="(emulate$qsp(-L|-RL|-LR|-R)$qsp(zsh|sh|ksh|bash)|\
+setopt$qsp(localoptions|))$qosp"
+# Options string, either opt1 opt2, or -o opt1 -o opt2
+local qopt="((#b)(${(~j.|.)${(@)${(@f)"$(<$ZSDIR/data/14_recommended_options)"}/\
+(#s)/"($qsp-o$qsp|$qsp)"}})#)"
+
+#print -r qpre: $qpre
+#print -r qopt:$qopt
