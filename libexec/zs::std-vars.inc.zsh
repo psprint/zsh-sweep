7730b7578b3269fb17fbcfba8dc82a9e1dbc5606
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2023-01-31 11:19:59 +0059

    fix: options

--- libexec/zs::std-vars.inc.zsh
+++ libexec/zs::std-vars.inc.zsh
@@ -19,15 +19,23 @@ local \
     qqstr5a='$[\(]([^\)]#)[\)]' \
     qqstr7='$[\{]([^\}]#)[\}]'
 
+# Multiple ids and mul.ids optional
+local qmid="(($qid)$qosp)##"
+local qomid="(($qid)$qosp)#"
+# The same with -o
+local qmoptid="((-o |)$qid$qosp)##"
+local qomoptid="((-o |)$qid$qosp)#"
+# String and str. optional
 local qstr="($qqstr1|$qqstr3$qqstr4|$qqstr5|$qqstr5a|$qqstr7|)"
 local qostr=$qstr[1,-2]'|([^[:space:]]#))'
+
 local -a qqprecmd=('\{' '\(' noglob command exec nocorrect builtin
                     '\&\&' '\|\|' '\|' '\&' if while )
 local qpre="($qosp(${(~j.|.)${qqprecmd[@]/(#e)/$qsp}}')|(#s)$qosp)"
 local qemu="(emulate$qsp(-L|-RL|-LR|-R)$qsp(zsh|sh|ksh|bash)|\
 setopt$qsp(localoptions|))$qosp"
 # Options string, either opt1 opt2, or -o opt1 -o opt2
-local qopt="((#b)(${(~j.|.)${(@)${(@f)"$(<$ZSDIR/data/14_recommended_options)"}/\
+local qropt="((#b)(${(~j.|.)${(@)${(@f)"$(<$ZSDIR/data/14_recommended_options)"}/\
 (#s)/"($qsp-o$qsp|$qsp)"}})#)"
 
 #print -r qpre: $qpre
